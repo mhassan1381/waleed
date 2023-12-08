5820a3f6bb2f09e0f6e6d6f418039ed9209da671
Hassan <hassan1381abid@gamil.com>
2023-12-08 18:11:43 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./Layout";
+
+function App() {
+  return (
+    <Layout>
+      <div><p> this is hassan</p></div>
+    </Layout>
+  );
+}
+
+export default App;
